735bab62afd0a114a6599a1bc2e45c50516e7eb7
Mastan Sayyad <sayyadmastan78@gmail.com>
2026-01-25 21:59:34 +0530

    feat: Added DatabaseException.java

---
+++ src/main/java/com/revature/revworkforce/exception/DatabaseException.java
@@ -0,0 +1,18 @@
+package com.revature.revworkforce.exception;
+
+/**
+ * Exception thrown when database operations fail
+ */
+
+public class DatabaseException extends Exception {
+    
+    private static final long serialVersionUID = 1L;
+    
+    public DatabaseException(String message) {
+        super(message);
+    }
+    
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
