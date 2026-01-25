96813fb45807fa99288dea5112f416f1928d6337
Mastan Sayyad <sayyadmastan78@gmail.com>
2026-01-25 21:58:56 +0530

    feat: Added AuthenticationException.java

---
+++ src/main/java/com/revature/revworkforce/exception/AuthenticationException.java
@@ -0,0 +1,18 @@
+package com.revature.revworkforce.exception;
+
+/**
+ * Exception thrown when authentication fails
+ */
+
+public class AuthenticationException extends Exception {
+    
+    private static final long serialVersionUID = 1L;
+    
+    public AuthenticationException(String message) {
+        super(message);
+    }
+    
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
